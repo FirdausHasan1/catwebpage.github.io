db082faa68f06a4a6107fa133c52a5d656825e1b
FirdausHasan1 <115791090+FirdausHasan1@users.noreply.github.com>
2023-02-19 21:05:09 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,11 @@
+var vid = document.getElementById("limitless");
+
+function one() {
+    document.getElementById("limitless").style.opacity = '1';
+    document.getElementById("content3").style.opacity = '1';
+    document.getElementById("opacity").style.opacity = '0';
+}
+
+function playVid() {
+    vid.play();
+}
